13bfa4356aebd89cb5186dc4aed423851f1b31ce
ferloz97 <ferloz97.gr@gmail.com>
2025-07-28 12:25:27 +0200

    Can execute a command line with only one command

--- main.c
+++ main.c
@@ -2,18 +2,25 @@
 #include <string.h>
 #include <unistd.h>
 #include "lib/parser.h"
+#include <sys/wait.h>
 
 // Constantes
 #define BUFF_IN 128
 
 // Variables Globales
-char pwd[1024];
+
 char* user = "user";
 char* pcname = "userpc";
-int exiting = 0;
+
 
 int main() {
+    char pwd[1024];
+    int exiting = 0;
     char buff_in[BUFF_IN];
+    tline* command_line = NULL;
+    pid_t pid;
+
+
     while (exiting == 0) {
         // Prompt
         getcwd(pwd,1024);
@@ -23,8 +30,23 @@ int main() {
         fgets(buff_in,BUFF_IN,stdin);
 
         // Analisis
+        command_line = tokenize(buff_in);
+        if (command_line == NULL) {
+            //  ERROR
+        }else {
+
+        }
+
 
         // Ejecucion
+        pid = fork();
+        if (pid < 0) {
+            //ERROR
+        }else if (pid == 0) {
+            execv(command_line->commands[0].filename,command_line->commands[0].argv);
+        }else {
+            waitpid(pid,NULL,0);
+        }
 
     }
 }
